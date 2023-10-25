23b635c96fc4d97f7da2536ad511a442e6f423d7
LiveNL <jordiwippert@live.nl>
2023-10-25 07:38:13 +0200

    Add working alpha-custom layout with header

---
+++ .config/nvim/lua/plugins/alpha-custom.lua
@@ -0,0 +1,67 @@
+-- local M = {
+-- 	"goolord/alpha-nvim",
+-- 	enabled = true,
+-- 	cond = vim.g.vscode == nil,
+-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+-- 	event = "VimEnter",
+-- }
+
+-- M.config = function()
+return function()
+	local headers = require("plugins.alpha-headers")
+	local theme = require("alpha.themes.theta")
+	local path_ok, plenary_path = pcall(require, "plenary.path")
+
+	if not path_ok then
+		return
+	end
+
+	math.randomseed(os.time())
+
+	-- Header
+	local function apply_gradient_hl(text)
+		local gradient = require("plugins.utils").create_gradient("#DCA561", "#658594", #text)
+
+		local lines = {}
+		for i, line in ipairs(text) do
+			local tbl = {
+				type = "text",
+				val = line,
+				opts = {
+					hl = "HeaderGradient" .. i,
+					shrink_margin = false,
+					position = "center",
+				},
+			}
+			table.insert(lines, tbl)
+
+			-- create hl group
+			vim.api.nvim_set_hl(0, "HeaderGradient" .. i, { fg = gradient[i] })
+		end
+
+		return {
+			type = "group",
+			val = lines,
+			opts = { position = "center" },
+		}
+	end
+
+	local function get_header(headers)
+		local header_text = headers[math.random(#headers)]
+		return apply_gradient_hl(header_text)
+	end
+
+	theme.config.layout = {
+		{ type = "padding", val = 4 },
+		get_header({
+			headers.header_lines,
+			headers.coolLines,
+			headers.robustLines,
+			headers.efficientLines,
+		}),
+	}
+
+	require("alpha").setup(theme.config)
+end
+
+-- return M
